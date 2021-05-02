f40720156fdda2b053ba831b9df5726c9b20836b
Max <maximilian.halanen@gmail.com>
2021-05-01 20:51:09 -0400

    Grid functionality
    made is so variables can be added and removed

--- calculator.gd
+++ calculator.gd
@@ -1,9 +1,33 @@
 extends Control
 
+onready var functions = get_node("/root/Functions")
+
 var symbol = ["A","B","C","/and","/or", "/not", "/xor", "/imp", "/dmp", "/par", "/del"]
 
 func _ready():
 	var button = $"HBoxContainer/VBoxContainer2/GridContainer".get_children()
+	var grid = $"HBoxContainer/VBoxContainer/GridContainer2".get_children()
+	
+	var B = [true, false, true, false]
+	var A = [true, true, false, false]
+	var andlist = functions.andOp(A,B)
+	var headers = [A, B, andlist]
+	var rows = 4
+	var cols = 8
+	
+	for i in len(headers):
+		for j in range(rows):
+			var text = ""
+			if headers[i][j]:
+				text =  "1"
+			else:
+				text = "0"
+			grid[j*cols + i].get_node("RichTextLabel").text = text
+		
+#	for i in len(grid):
+#		grid[i].get_node("RichTextLabel").text = str(i)
+#
+	
 	
 	for i in range(len(button)):
 		button[i].connect("pressed", self, "_on_Button_pressed", [i])
